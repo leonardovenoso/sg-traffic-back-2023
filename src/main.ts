dca60d8de97171b00415758c1505b90388313b86
Leonardo <leonardo.venoso@live.com>
2023-04-17 18:06:11 +0800

    Added server with validation pipe configuration

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { LocationModule } from './location.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(LocationModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  await app.listen(3001);
+}
+bootstrap();
